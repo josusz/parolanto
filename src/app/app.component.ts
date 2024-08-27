d9d6f4965e12ba1c2ebfaa27185752ba6f1ef613
josusz <josuhcarvalho@gmail.com>
2024-08-27 18:37:21 -0300

    iniciando o desenvolvimento da tela de cadastro de usuários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsuarioComponent } from "./usuario/usuario.component";
+import { RegistrarUsuarioComponent } from "./registrar-usuario/registrar-usuario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsuarioComponent],
+  imports: [RouterOutlet, UsuarioComponent, RegistrarUsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
