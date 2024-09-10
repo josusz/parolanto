188a1ff41f1db0acb598d60a9f2b391891068500
josusz <josuhcarvalho@gmail.com>
2024-09-10 18:39:21 -0300

    iniciando a estrutura da página logar-usuario.component.html

--- src/app/logar-usuario/logar-usuario.component.ts
+++ src/app/logar-usuario/logar-usuario.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-logar-usuario',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './logar-usuario.component.html',
   styleUrl: './logar-usuario.component.css'
 })
