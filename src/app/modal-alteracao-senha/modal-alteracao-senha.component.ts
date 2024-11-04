c757d9020c103e5041d345f875229a36858941b1
josusz <josuhcarvalho@gmail.com>
2024-11-04 18:38:16 -0300

    adição do item Alterar senha e criação do componente: modal-alteracao-senha

---
+++ src/app/modal-alteracao-senha/modal-alteracao-senha.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-alteracao-senha',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-alteracao-senha.component.html',
+  styleUrl: './modal-alteracao-senha.component.css'
+})
+export class ModalAlteracaoSenhaComponent {
+
+}
