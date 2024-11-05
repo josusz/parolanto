52a219c39844d5dbcc6df4063dcf1480bf4c19d3
josusz <josuhcarvalho@gmail.com>
2024-11-05 16:22:41 -0300

    função para abrir o componente modal-alteracao-senha após selecionar o item Alterar senha

--- src/app/modal-alteracao-senha/modal-alteracao-senha.component.ts
+++ src/app/modal-alteracao-senha/modal-alteracao-senha.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-modal-alteracao-senha',
@@ -9,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class ModalAlteracaoSenhaComponent {
 
+  private modalElement: HTMLElement | null = null; //inicialização com null
+  private bsModal?: Modal; //uso de undefined como padrão
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.modalElement = document.getElementById('alterarSenhaModal') as HTMLElement;
+    if (this.modalElement) {
+      this.bsModal = new Modal(this.modalElement);
+    }
+  }
+
+  openModal() {
+    if (this.bsModal) {
+      this.bsModal.show();
+    }
+  }
 }
