93107932a4cb43754764ef875da2577eb3c9af57
josusz <josuhcarvalho@gmail.com>
2024-11-09 20:27:30 -0300

    closes #19: perfil-usuario concluído

--- src/app/perfil-usuario/perfil-usuario.component.ts
+++ src/app/perfil-usuario/perfil-usuario.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { UsuarioService } from '../usuario.service';
 import { NavbarInterativoComponent } from "../navbar-interativo/navbar-interativo.component";
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from "../modal/modal.component";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-perfil-usuario',
   standalone: true,
-  imports: [NavbarInterativoComponent, CommonModule, RouterModule],
+  imports: [NavbarInterativoComponent, CommonModule, RouterModule, ModalComponent],
   templateUrl: './perfil-usuario.component.html',
   styleUrl: './perfil-usuario.component.css'
 })
@@ -15,8 +17,12 @@ export class PerfilUsuarioComponent implements OnInit {
   usuario: any;
   projetos: any[] = [];
   id!: number;
+  errorMessages: string[] = [];
+  usuarioEncontrado: boolean = true;
 
-  constructor(private route: ActivatedRoute, private usuarioService: UsuarioService) { }
+  @ViewChild(ModalComponent) modalComponent?: ModalComponent;
+
+  constructor(private route: ActivatedRoute, private usuarioService: UsuarioService, private location: Location) { }
 
   ngOnInit(): void {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
@@ -28,10 +34,22 @@ export class PerfilUsuarioComponent implements OnInit {
       next: (data) => {
         this.usuario = data;
         this.projetos = data.projetos;
+        this.usuarioEncontrado = true;
       },
       error: (err) => {
-        console.error('Erro ao carregar dados do usuário:', err);
+        this.errorMessages = [err.error.message || 'Erro ao carregar dados do usuário.'];
+        this.usuarioEncontrado = false;
+        this.openModal('error', 'Atenção!');
       }
     });
+  }
+
+  openModal(modalType: 'error', title: string) {
+    if (this.modalComponent) {
+      this.modalComponent.modalType = modalType;
+      this.modalComponent.title = title;
+      this.modalComponent.messages =this.errorMessages;
+      this.modalComponent.openModal();
+    }
   }
 }
